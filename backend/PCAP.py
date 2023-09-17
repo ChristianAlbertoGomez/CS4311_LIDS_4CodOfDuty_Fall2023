83473784eeee0c3d5a499ea86c3f889a06289526
sergiosaw3 <myfavred@gmail.com>
2023-09-17 12:27:39 -0600

    Update PCAP.py

--- backend/PCAP.py
+++ backend/PCAP.py
@@ -2,13 +2,13 @@ from tabulate import tabulate
 
 # Define your data as a list of lists (each inner list represents a row)
 data = [
-    ["Alice", 28, "Engineer"],
-    ["Bob", 35, "Designer"],
-    ["Charlie", 22, "Manager"],
+    ["12:40", "192.102.1.2", "TCP","18","4000 -> 80 [SYN]..."],
+    ["06:25", "187.004.3.5", "TCP","90","2480 -> 80 [SYN]..."],
+    ["14:41", "100.821.5.1", "TCP","92","2480 -> 80 [SYN]..."],
 ]
 
 # Define the headers for your table
-headers = ["Name", "Age", "Occupation"]
+headers = ["Time", "Source", "Destination","Protocol","Length","Description"]
 
 # Use the tabulate function to format the table
 table = tabulate(data, headers, tablefmt="grid")

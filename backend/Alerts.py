223452854e638e86f1e0277cf51faef817f63c49
sergiosaw3 <myfavred@gmail.com>
2023-09-29 15:57:21 -0600

    Fix table, started createAlerts

--- backend/Alerts.py
+++ backend/Alerts.py
@@ -13,20 +13,19 @@ def alerts():
 if __name__=="__main__":
     app.run(debug=True)
 
+def createAlerts(packet,whitelist):
+    packet['header']
+    return
+
 # Define your data as a list of lists (each inner list represents a row)
 def alertTable():
-    data = [
-        [2, 11.6578, "192.128.0.1", 80, "Unknown host ping"],
-        [3, 11.6578, "193.127.0.2", 27, "port scan"],
-        [1, 11.6578, "192.128.0.1", 80, "Fail login attempt"],
-        [2, 11.6578, "193.124.0.3", 4040, "Unknown host ping"]
-    ]
+    data = alerts()
 
     # Define the headers for your table
-    headers = ["Level", "Time", "IP", "Port", "Description"]
+    #headers = ["Level", "Time", "IP", "Port", "Description"]
 
     # Use the tabulate function to format the table
-    table = tabulate(data, headers, tablefmt="grid")
+    table = tabulate(data, headers='keys', tablefmt="grid")
 
     # Print the formatted table
     print(table)

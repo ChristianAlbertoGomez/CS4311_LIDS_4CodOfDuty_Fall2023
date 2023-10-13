2ea0e79848a949fc92872362fed4da2684a3ebe9
Courtney <clwoods2@miners.utep.com>
2023-10-13 12:24:57 -0600

    finished export methods

--- backend/export.py
+++ backend/export.py
@@ -1,8 +1,7 @@
 import json
-def create_alert(packet, whitelist):
-    res={'level':'Mid','time':packet['header']['time'],'port':packet['header']['srcPort'],'description':'Placeholder','ipSource':packet['header']['srcIP'],'ipDestination':packet['header']['dstIP'],'date':'placeholder','details':'placeholder'}
-    alerts.append(res)
-    
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+
 
 def exportJSON(alerts):
     x = json.dumps(alerts, indent=4)
@@ -11,4 +10,24 @@ def exportJSON(alerts):
         outfile.write(x)
         outfile.close()
 
+def exportXML(alerts):
+    root = ET.Element("root")
+
+    for alert in alerts:
+        alertElement = ET.SubElement(root, "alert")
+        for i,j in alert.items():    
+            ET.SubElement(alertElement, i).text = str(j)
+    xmlstr = minidom.parseString(ET.tostring(root)).toprettyxml(indent="   ")
+    with open("alerts.xml", "w") as f:
+        f.write(xmlstr)
+        f.close()
+
+def exportCSV(alerts):
+    header = ['level','time','port','description','ipSource','ipDestination','date','details']
     
+    with open('alerts.csv', 'w') as f:
+        f.write(','.join(header) + '\n')
+        for alert in alerts:
+            for i,j in alert.items():
+                f.write(str(j) + ',')
+            f.write('\n')

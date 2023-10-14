b22000bd8c20a9cf3dc21dd90cf60c7ab33ce497
Courtney <clwoods2@miners.utep.com>
2023-10-14 14:45:38 -0600

    hold

--- backend/main.py
+++ backend/main.py
@@ -13,24 +13,24 @@ if __name__ == "__main__":
       # Call connect_server with the server_info dictionary and capture_interface
       lids.connect_server(server_info)
 
-  while True:
-      try:
-          print('Welcome, please select one of the following options (1-6)')
-          userInp = int(input("1) Configuration File\n2) Show PCAP\n3) Select PCAP\n4) Alerts\n5) Help\n6) Exit\n"))
-          if userInp > 6 or userInp < 1:
-              print("Please select a valid option")
-          elif userInp == 1:
-              setConfigFile()
-          elif userInp == 2:
-              pcapTable() 
-          elif userInp == 3:
-              print("You'll be able to search for a specific PCAP")
-          elif userInp == 4:
-              alertTable()
-          elif userInp == 5:
-              print("You'll find help options here")
-          elif userInp == 6:
-              print("Have a nice day")
-              break
-      except ValueError:
-          print("Invalid input. Please enter a number (1-6).")
+  # while True:
+  #     try:
+  #         print('Welcome, please select one of the following options (1-6)')
+  #         userInp = int(input("1) Configuration File\n2) Show PCAP\n3) Select PCAP\n4) Alerts\n5) Help\n6) Exit\n"))
+  #         if userInp > 6 or userInp < 1:
+  #             print("Please select a valid option")
+  #         elif userInp == 1:
+  #             setConfigFile()
+  #         elif userInp == 2:
+  #             pcapTable() 
+  #         elif userInp == 3:
+  #             print("You'll be able to search for a specific PCAP")
+  #         elif userInp == 4:
+  #             alertTable()
+  #         elif userInp == 5:
+  #             print("You'll find help options here")
+  #         elif userInp == 6:
+  #             print("Have a nice day")
+  #             break
+  #     except ValueError:
+  #         print("Invalid input. Please enter a number (1-6).")

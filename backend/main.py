922ff056ef3fa1cede8962b01054e3ce59122d65
Christopher Porras <98921626+ceporras2@users.noreply.github.com>
2023-10-14 14:45:04 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,36 +1,40 @@
-from config import *
-from Alerts import *
-from PCAP import *
+# from config import *
+# from Alerts import *
+# from PCAP import *
 import LIDS as lids
 
 if __name__ == "__main__":
-  # Set configuration file
-  CONFIG_FILE = 'config_file.xml' 
-  print(f"Using configuration file: {CONFIG_FILE}")
-  server_info, net_systems = lids.ingest_config(CONFIG_FILE)
+    # Need config file from frontend sent here
+    CONFIG_FILE = 'config_file.xml'
+    
+    #print(f"Using configuration file: {CONFIG_FILE}")
+    server_info, net_systems, system_info = lids.ingest_config(CONFIG_FILE)
 
-  if server_info is not None and net_systems is not None:
-      # Call connect_server with the server_info dictionary and capture_interface
-      lids.connect_server(server_info)
+    # if server_info is not None and net_systems is not None:
+    #     # Call connect_server with the server_info dictionary and system information
+    #     lids.connect_server(server_info, system_info)
 
-  while True:
-      try:
-          print('Welcome, please select one of the following options (1-6)')
-          userInp = int(input("1) Configuration File\n2) Show PCAP\n3) Select PCAP\n4) Alerts\n5) Help\n6) Exit\n"))
-          if userInp > 6 or userInp < 1:
-              print("Please select a valid option")
-          elif userInp == 1:
-              setConfigFile()
-          elif userInp == 2:
-              pcapTable() 
-          elif userInp == 3:
-              print("You'll be able to search for a specific PCAP")
-          elif userInp == 4:
-              alertTable()
-          elif userInp == 5:
-              print("You'll find help options here")
-          elif userInp == 6:
-              print("Have a nice day")
-              break
-      except ValueError:
-          print("Invalid input. Please enter a number (1-6).")
+    while True:
+        try:
+            print('Welcome, please select one of the following options (1-6)')
+            userInp = int(input("1) Configuration File\n2) Show PCAP\n3) Select PCAP\n4) Alerts\n5) Help\n6) Exit\n"))
+            if userInp > 6 or userInp < 1:
+                print("Please select a valid option")
+            elif userInp == 1:
+                print("Please select a valid option")
+                # setConfigFile()
+            elif userInp == 2:
+                print("Please select a valid option")
+                # pcapTable() 
+            elif userInp == 3:
+                print("You'll be able to search for a specific PCAP")
+            elif userInp == 4:
+                print("Please select a valid option")
+                # alertTable()
+            elif userInp == 5:
+                print("You'll find help options here")
+            elif userInp == 6:
+                print("Have a nice day")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number (1-6).")

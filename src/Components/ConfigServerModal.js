04e7ec9ba4c0fc85a093dfc4c2418b8fd9453c95
ChristianAlbertoGomez <cagomez15@miners.utep.edu>
2023-10-22 17:36:43 -0600

    Added Config Server option

---
+++ src/Components/ConfigServerModal.js
@@ -0,0 +1,51 @@
+import React, { useRef } from 'react';
+import './CSS Files/ConfigServerModal.css';
+import { BsFileText } from 'react-icons/bs';
+
+const ConfigServerModal = ({ onClose }) => {
+  const fileInputRef = useRef(null); // Crea una referencia para el elemento input
+
+  const handleFileSubmit = (event) => {
+    event.preventDefault();
+    const selectedFile = fileInputRef.current.files[0];
+    console.log(selectedFile);
+    
+    window.location.href = '/home';
+  };
+
+  const browseForFile = () => {
+    fileInputRef.current.click(); // Accede al elemento input a través de la referencia
+  };
+
+  return (
+    <div className='modal-overlay'>
+      <div className='modal-content'>
+        <button className='close-button' onClick={onClose}>X</button>
+        <BsFileText className='modal-icon' />
+        <div className='modal-text'>Config_File.xml</div>
+        <button className='modal-start-button' onClick={handleFileSubmit}>
+          Start Server
+        </button>
+        <form onSubmit={handleFileSubmit}>
+          <input
+            type='file'
+            name='fileInput'
+            id='fileInput'
+            ref={fileInputRef} // Asocia la referencia con el elemento input
+            style={{ display: 'none' }}
+          />
+          <button
+            type='button'
+            onClick={browseForFile}
+            className='modal-browser-button'
+          >
+            Browse
+          </button>
+          <button type='submit' style={{ display: 'none' }} />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ConfigServerModal;
--- src/Components/NetworkPage.js
+++ src/Components/NetworkPage.js
@@ -1,34 +1,52 @@
-// NetworkPage.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import AlertTable from './AlertTable';
 import KnownDevices from './KnownDevices';
 import UnknownDevices from './UnknownDevices';
 import './CSS Files/NetworkPage.css';
+import { BsFileText } from 'react-icons/bs';
+import ConfigServerModal from './ConfigServerModal';
 
 const NetworkPage = () => {
+  const [isScreenVisible, setScreenVisible] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleScreen = () => {
+    setScreenVisible(!isScreenVisible);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
     <div className='network-page'>
       <div>
-        <Sidebar/>
+        <Sidebar />
         <div className='header'>
-        Network Map
+          <div className='header-content'>
+            Network Map
+          </div>
         </div>
         <main className='mainBody'>
+          <div className='modal-button' onClick={toggleModal}>
+            <BsFileText className='icon-button large-icon' />
+            <div className='icon-label'>Config Server</div>
+          </div>
           <div className='tables'>
             <div className='table-1'>
-              <KnownDevices/>
+              <KnownDevices />
             </div>
             <div className='table-2'>
               <UnknownDevices />
             </div>
           </div>
-          <div className='AlertTable'>
-            <AlertTable/>
+          <div className={`AlertTable ${isScreenVisible ? 'visible' : ''}`}>
+            <AlertTable />
           </div>
         </main>
       </div>
+      {showModal && <ConfigServerModal onClose={toggleModal} />}
     </div>
   );
 };

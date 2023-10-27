d84c9a4f2c21561963488649e438f16b72a2661c
car70scep <ccepeda122@gmail.com>
2023-10-27 16:07:24 -0600

    Send files f*kin works

--- backend/test.py
+++ backend/test.py
@@ -1,20 +1,28 @@
-# from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
 
-# app = Flask(__name__)
 
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     if 'file' not in request.files:
-#         return jsonify({'message': 'No file part'})
+app = Flask(__name__)
+CORS(app,origins=["http://localhost:3000"] )
 
-#     file = request.files['file']
+@app.route('/', methods=['POST', 'GET'])
+def handle_file_upload():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
 
-#     if file.filename == '':
-#         return jsonify({'message': 'No selected file'})
+    file = request.files['file']
 
-#     # Process the uploaded file as needed (e.g., save it to the server or perform some operations)
-#     # In this example, we're just returning the file name
-#     return jsonify({'message': 'File uploaded successfully', 'filename': file.filename})
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+    # Optionally, you can save the uploaded file to a directory.
+    # To save it in a directory called 'uploads', you can use the following code:
+    # file.save('uploads/' + secure_filename(file.filename))
+
+    # You can then process the file or return a response as needed.
+    print(file)
+    return jsonify({'message': 'File uploaded successfully'})
+  
+
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', port=5000)
--- src/SetConfigFile.js
+++ src/SetConfigFile.js
@@ -2,35 +2,78 @@ import React from "react";
 import './SetConfigFile.css';
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
 
 
+
+// const SetConfigFile = () => {
+
+//   const [selectedFile, setSelectedFile] = useState(null);
+
+//   const handleFileChange = (event) => {
+//     setSelectedFile(event.target.files[0]);
+//     const submit = document.getElementById("targetInput");
+//     submit.removeAttribute("disabled");
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+    
+    
+//     console.log('Selected file:', selectedFile);
+  
+//   };
 
 const SetConfigFile = () => {
 
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [ file, setFile ] = useState(null);
+  const [ progress, setProgress ] = useState({started: false, pc: 0});
+  const [ msg, setMsg ] = useState(null);
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-    const submit = document.getElementById("targetInput");
-    submit.removeAttribute("disabled");
-  };
+  function handleUpload(){
+    if(!file){
+      setMsg("No file uploaded");
+      return;
+    }
+    const fd = new FormData();
+    fd.append('file', file);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    
-    
-    console.log('Selected file:', selectedFile);
-  
-  };
+    setMsg("Uploading...");
+    setProgress(prevState => {
+      return {...prevState, started: true}
+    })
+    axios.post('http://127.0.0.1:5000', fd, {
+      // 'http://httpbin.org/post'
+      onUploadProgress: (progressEvent) => {setProgress(prevState => {
+        return { ...prevState, pc: progressEvent.progress*100}
+      }) },
+      headers: {
+        "Custom-Header": "value",
+      }
+    })
+    .then(res => {
+      setMsg("Upload Succesful");
+      console.log(res.data);
+      const submit = document.getElementById("targetInput");
+      submit.removeAttribute("disabled");
+    })
+    .catch(err =>{ 
+      setMsg("Upload Failed");
+      console.log(err)
+    });
+  }
+
 
   return (
     <div className="config">
       <div className="title">Welcome to LIDS</div>
       <div className="title-2">Please select the Configuration File</div>
-      <form onSubmit={handleSubmit}>
-        <input type="file" accept=".xml" onChange={handleFileChange} id="fileInput" />
-        <Link to="/home">
-          <input type="submit" value="Submit" id="targetInput" className="submit-button" disabled={selectedFile === null} />
+      <form>
+        <input type="file" accept=".xml" onChange={ (e) => { setFile(e.target.files[0]) } } id="fileInput" />
+        <Link to="/home"> 
+          <input type="submit" value="Submit" id="targetInput" className="submit-button" onClick={ handleUpload} disabled={file === null}/>
+          {progress.started && <progress max="100" value={progress.pc}></progress>}
+          {msg && <span>{msg}</span>}
         </Link>
       </form>
     </div>
